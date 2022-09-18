ebcafe1b70cf23c5149b397d5b8d666af872a396
Abhay Madaan <78895271+AbhayMadaan9@users.noreply.github.com>
2022-09-18 18:09:52 +0530

    navbar_addition

--- my-app/src/Componenets/Navbar.js
+++ my-app/src/Componenets/Navbar.js
@@ -1,16 +1,38 @@
-import React from 'react'
+
+// import 'bootstrap/dist/css/bootstrap.css'
 import '../Components_styling/Navbar_style.css'
 
 export default function Navbar() {
   return (
     <>
     <div className="container">
+
+      <div className="container1">
         <ul>
-            <li>logo</li>
-            <li>Heading</li>
-            <li>dropdown</li>
+
+          <li id='logo'>
+            <img src="logo.png" alt="logo " />
+          </li>
+
+          <li id='heading'>
+            <p>Training & Placement Cell</p>
+          </li>
+
         </ul>
-    </div>
+        </div>
+        
+        <div className="container2">
+          <ul>
+            <li><a href='/'>Login</a></li>
+            <li><a href='/'>About</a></li>
+            <li><a href='/'>Genconian</a></li>
+            <li><a href='/'>Contact Details</a></li>
+            <li><a href='/'>More</a></li>
+          </ul>
+        </div>
+
+      </div>
+
     </>
   )
 }
